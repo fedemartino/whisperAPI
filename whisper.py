28406dba5a4d0bba87da5e5cb107df029fe9940f
Federico Martino <fedemartino@gmail.com>
2023-06-21 13:34:25 -0300

    key in ignored file

--- whisper.py
+++ whisper.py
@@ -20,7 +20,7 @@ def text_to_chunks(text):
   for sentence in sentences.sents:
     chunk_total_words += len(sentence.text.split(" "))
 
-    if chunk_total_words > 1000:
+    if chunk_total_words > 1300:
       chunks.append([])
       chunk_total_words = len(sentence.text.split(" "))
 
@@ -30,23 +30,24 @@ def text_to_chunks(text):
 
 #summarize text in no more than 3 sentences
 def summarize_text(text):
-  prompt = f"Summarize the following text in no more than 3 sentences:\n{text}"
+    #prompt = f"The follwing text is a summary of a phone conversation between a customer and an account manager. Summarize the text in 4 sentences or less and classify the call into either a complaint or not complaint:\n{text}"
+    #prompt = f"Resume el siguiente texto en 5 frases o menos:\n{text}"
+    prompt = f"Summarize the following text in 5 sentences or less:\n{text}"
 
-  response = openai.Completion.create(
-      engine="text-davinci-003", 
-      prompt=prompt,
-      temperature=0.3, 
-      max_tokens=250, # = 112 words
-      top_p=1, 
-      frequency_penalty=0,
-      presence_penalty=1
-  )
-
-  return response["choices"][0]["text"]
+    response = openai.Completion.create(
+        engine="text-davinci-003", 
+        prompt=prompt,
+        temperature=0.3, 
+        max_tokens=250, # = 112 words
+        top_p=1, 
+        frequency_penalty=0,
+        presence_penalty=1
+    )
+    return response["choices"][0]["text"]
 
 #summarize text in no more than 3 sentences
 def classify_sentiment(text):
-  prompt = f"The follwing text is a summary of a phone conversation between a customer and an account manager. Classify the sentiment of the call into either positive, negative or neutral:\n{text}"
+  prompt = f"Answering with a single word (satisfied or unsatisfied) to indicate if either of the people in the text were not satisfied with a service received at any point in the story:\n{text}"
 
   response = openai.Completion.create(
       engine="text-davinci-003", 
@@ -69,7 +70,9 @@ def get_transcript():
     file.close()
 
 # Load your API key from an environment variable or secret management service
-openai.api_key = "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"
+keyfile = open("openai_api.key", "r")
+openai.api_key = keyfile.readline()
+keyfile.close()
 
 #get_transcript()
 
@@ -84,12 +87,13 @@ chunk_sentiments = []
 
 for chunk in chunks:
   chunk_summary = summarize_text(" ".join(chunk))
-  chunk_sentiment = classify_sentiment(" ".join(chunk))
+  #chunk_sentiment = classify_sentiment(" ".join(chunk))
   chunk_summaries.append(chunk_summary)
-  chunk_sentiments.append(chunk_sentiment)
+  #chunk_sentiments.append(chunk_sentiment)
 
 summary = " ".join(chunk_summaries)
-sentiment = " ".join(chunk_sentiment)
-print(input)
+#sentiment = " ".join(chunk_sentiment)
+sentiment = classify_sentiment(summary)
+#print(input)
 print(summary)
 print(sentiment)
